81addb9084f31cfd9aa06e98b2db9e734eee6f82
Daniel Holland <danhol1989@gmail.com>
2022-04-18 22:15:40 -0700

    removed Reaction.js, and added its code to Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
-const userSchema = require('./User');
 const reactionSchema = require('./Reaction');
+const moment = require('moment');
 
 // Schema to create Thought model
 const thoughtSchema = new Schema(
@@ -25,6 +25,40 @@ const thoughtSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Schema to create Reaction model
+const reactionSchema = new Schema(
+  {
+    reactionId:
+        {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: [
+        userSchema,
+        {
+            type: String,
+            required: true,
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+      },
+  },
+  {
+    toJSON: {
       getters: true,
     },
     id: false,
